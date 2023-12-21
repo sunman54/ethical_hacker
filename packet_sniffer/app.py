c8e1715db46060f920bbc907ff5ebcbd610f31e8
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2023-12-21 11:36:03 +0300

    sniffer updated

--- packet_sniffer/app.py
+++ packet_sniffer/app.py
@@ -3,13 +3,24 @@ from scapy.layers import http
 
 
 def sniff(interface):
-    sc.sniff(iface=interface, store=false, prn=process_sniffed_packet)
+    sc.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 
 def process_sniffed_packet(packet):
-    if packet.haslayer(http.HTTPRequest) and packet.haslayer(sc.Raw):
-        print(packet[sc.Raw].load)
+    if packet.haslayer(http.HTTPRequest):
+        url = get_url(packet)
+        print(f"HTTP Request: {url}")
+
+        if packet.haslayer(sc.Raw):
+            load = get_load(packet)
+            print(f"Raw Data: {load}")
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
 
-sniff("eth0")
+
+def get_load(packet):
+    return packet[sc.Raw].load
+
+sniff("eno1")
