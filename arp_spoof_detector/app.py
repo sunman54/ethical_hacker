ebec8934dfe1ed22cee87acea67db449d494b7dc
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2024-01-03 17:01:48 +0300

    ARP Sniff Detector added

---
+++ arp_spoof_detector/app.py
@@ -0,0 +1,27 @@
+import scapy.all as scapy
+
+def get_mac(ip):    
+    arp_request = sc.ARP(pdst = ip)
+    broadcast = sc.Ether(dst = 'ff:ff:ff:ff:ff:ff')
+    arp_request_broadcast = broadcast/arp_request # creating the packet
+    answered_list, unanswered_list = sc.srp(arp_request_broadcast, timeout=2, verbose=False) #sending the packet
+
+    return answered_list[0][1].hwsrc
+
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn= process_sniffed_packet)
+
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(scapy.ARP) and packet[scapy.ARP].op == 2:
+        real_mac = get_mac(packet[scapy.ARP].psrc)
+        response_mac = packet[scapy.ARP].hwsrc
+
+        if real_mac != response_mac:
+            print('[x] You are UNDER ATTACK !!!')
+
+    
+
+sniff('eth0')
