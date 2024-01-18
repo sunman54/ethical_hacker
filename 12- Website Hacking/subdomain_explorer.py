8e1131fdd005fe7f04327de116fca0220d42895b
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2024-01-18 23:03:18 +0300

    subdomain explorer updated

--- 12- Website Hacking/subdomain_explorer.py
+++ 12- Website Hacking/subdomain_explorer.py
@@ -3,14 +3,15 @@ import requests
 
 def make_request(url):
     try:
-        return requests.get('http://' + url )
-    
+        return requests.get('http://' + url)
+
     except requests.exceptions.ConnectionError:
         pass
-    
+
+
 target_url = 'google.com'
 
-with open('./subdomains-wodlist.txt', 'r') as file:
+with open('./data/subdomains-wodlist.txt', 'r') as file:
     for line in file:
         word = line.strip()
 
