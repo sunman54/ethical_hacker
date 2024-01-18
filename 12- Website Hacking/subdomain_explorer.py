df50540b2ededb938a4bd7e5b01d0674d1bc6074
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2024-01-18 22:56:18 +0300

    subdomain explorer added

---
+++ 12- Website Hacking/subdomain_explorer.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def make_request(url):
+    try:
+        return requests.get('http://' + url )
+    
+    except requests.exceptions.ConnectionError:
+        pass
+    
+target_url = 'google.com'
+
+with open('./subdomains-wodlist.txt', 'r') as file:
+    for line in file:
+        word = line.strip()
+
+        test_url = word + '.' + target_url
+        response = make_request(test_url)
+
+        if response:
+            print(f'[+] Subdomain discovered : {test_url}')
