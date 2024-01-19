7ba9adbd2d5110e2d0ba8dfc06e6cf066a05742a
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2024-01-19 22:58:12 +0300

    link extracter added

---
+++ 12- Website Explorers/link_extracter.py
@@ -0,0 +1,17 @@
+import requests
+import re
+
+def extract_links_from(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return re.findall('(?:href=")(.*?)"', response.text)
+    else:
+        print(f"Failed to retrieve content from {url}. Status code: {response.status_code}")
+        return []
+
+target_url = 'https://google.com'
+
+href_links = extract_links_from(target_url)
+
+for link in href_links:
+    print(link)
